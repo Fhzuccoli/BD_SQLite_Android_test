1898f650f8786397fdd4a33c6d55f2b88b7bd208
Fhzuccoli <fhzuccoli@gmail.com>
2021-07-28 16:43:35 -0300

    BD criado na MainActivity

--- app/src/main/java/com/example/bancodedadossqlite/MainActivity.java
+++ app/src/main/java/com/example/bancodedadossqlite/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bancodedadossqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            //criar bd
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app", 0, null);
+
+            //criar tabela
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT (3))");
+
+            //inserir valores
+            bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Ful', 30)");
+            bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Sicl', 35)");
+
+            //recuperar pessoas
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+
+            //indices da tabela
+            int indiceNome = cursor.getColumnIndex("nome");
+            int indiceIdade = cursor.getColumnIndex("idade");
+
+            while (cursor != null){
+                Log.i("RESULTADO - nome: ", cursor.getString(indiceNome));
+                Log.i("RESULTADO - idade: ", cursor.getString(indiceIdade));
+            }
+
+        }catch (Exception e){
+            e.fillInStackTrace();
+        }
     }
 }
