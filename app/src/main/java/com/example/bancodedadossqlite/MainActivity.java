fcfc295e729b64387ecb0513b09d534b1a4d64d3
Fhzuccoli <fhzuccoli@gmail.com>
2021-07-30 17:40:34 -0300

    registro deletado

--- app/src/main/java/com/example/bancodedadossqlite/MainActivity.java
+++ app/src/main/java/com/example/bancodedadossqlite/MainActivity.java
@@ -20,15 +20,21 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", 0, null);
 
             //criar tabela
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT (3))");
+            bancoDados.execSQL
+                    ("CREATE TABLE IF NOT EXISTS pessoas (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "nome VARCHAR," +
+                    "idade INT (3))");
 
             //inserir valores
             bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Ful', 30)");
             bancoDados.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Sicl', 35)");
+            bancoDados.execSQL("UPDATE pessoas SET idade = 19 WHERE nome = 'Ful'");
+            bancoDados.execSQL("DELETE FROM pessoas WHERE id = 1");
 
             //recuperar pessoas - filtros
             String consulta =
-                    "SELECT nome, idade" +
+                    "SELECT id, nome, idade" +
                     "FROM pessoas " +
                     "WHERE nome = 'Ful' AND idade = 30";
 
@@ -37,13 +43,15 @@ public class MainActivity extends AppCompatActivity {
             //indices da tabela
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
+            int indiceId = cursor. getColumnIndex("id");
 
             cursor.moveToFirst();
 
             while (cursor != null){
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
-                Log.i("RESULTADO - nome ", nome + " / idade: " + idade);
+                String id = cursor.getString(indiceId);
+                Log.i("RESULTADO - id ", id + " / nome: " + nome + " / idade: " + idade);
 
                 cursor.moveToNext();
             }
